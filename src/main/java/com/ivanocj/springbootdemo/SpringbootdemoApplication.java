04a3bef331bb433ab1eb1c99b04f822bf8b9eb93
Ivan <ivan@hackintosh.local>
2020-12-23 20:51:09 -0300

    Initial commit

---
+++ src/main/java/com/ivanocj/springbootdemo/SpringbootdemoApplication.java
@@ -0,0 +1,32 @@
+package com.ivanocj.springbootdemo;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Properties;
+
+@SpringBootApplication
+@RestController
+@Slf4j
+public class SpringbootdemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootdemoApplication.class, args);
+    }
+
+    @GetMapping("/greet")
+    public String hello(@RequestParam(value = "name", defaultValue = "World") String name, @RequestHeader(value = "User-Agent") String userAgent) {
+        return String.format("Hi %s!", name);
+    }
+
+    @GetMapping("/")
+    Properties properties() {
+        return System.getProperties();
+    }
+}
---
+++ src/main/java/com/ivanocj/springbootdemo/model/Customer.java
@@ -0,0 +1,45 @@
+package com.ivanocj.springbootdemo.model;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+
+@Document(collection = "Customer")
+public class Customer {
+    @Id
+    private String id;
+    @Indexed(name = "custName")
+    private String name;
+    private String description;
+
+    public Customer(String id, String name, String description) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
